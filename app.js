0704fec36f90f2eb6753712fe2b21aaaf9009aeb
rpsohag <rpsohag.bd@gmail.com>
2022-05-15 20:32:54 +0600

    javascript MERN stack batch 02 id-1758

---
+++ app.js
@@ -0,0 +1,45 @@
+/**
+ * 01. Create an email pattern 
+ */
+
+/*
+ let email = "rpsohag.bd@gmail.com";
+ let email_pattern = /^[a-z0-9._]*@[a-z]{2,10}\.[a-z]{2,8}$/;
+ console.log(email_pattern.test(email));
+*/
+
+/**
+ * 02. Create a  username pattern
+ */
+/*
+  let username ="rpsohag.bd";
+  let username_pattern = /^[a-z0-9._]{6,15}$/;
+  console.log(username_pattern.test(username));
+*/
+
+/**
+ * 03. Create a Bangladeshi phone number pattern
+ */
+/*
+  let phone_number = "+8801931602886"
+  let phone_number_pattern = /^(\+8801|01)[0-9]{9}$/;
+  console.log(phone_number_pattern.test(phone_number));
+*/
+
+/**
+ * 04. Create a password pattern
+ */
+/*
+  let password = "@This!s*&%wRd";
+  let password_pattern =/^[a-zA-Z0-9 ~!@#$%^&*()<>[{};.?]{6,32}$/;
+  console.log(password_pattern.test(password));
+*/
+
+/**
+ * 05. Create a zipcode pattern  
+ */
+/*
+  let zip_code = "1229";
+  let zip_code_pattern = /^[0-9]{4}$/;
+  console.log(zip_code_pattern.test(zip_code));
+  */
